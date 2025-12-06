b79a9345aa77038b48d9c7031eb873eb9209c191
alireza <theethanwardhere@gmail.com>
2025-12-06 19:57:26 +0330

    Structure

---
+++ src/main/java/com/ArcadeBoot/WaterTask/WaterTaskApplication.java
@@ -0,0 +1,13 @@
+package com.ArcadeBoot.WaterTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterTaskApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(WaterTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ArcadeBoot/WaterTask/WaterTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArcadeBoot.WaterTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
